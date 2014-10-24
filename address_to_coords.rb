b593af8359fef23ded843ce9dc375a27629c6241
rafinulman <rafinulman@gmail.com>
2014-10-24 11:37:10 -0500

    Added the Google coordinates

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -10,19 +10,21 @@ the_address = gets.chomp
 url_safe_address = URI.encode(the_address)
 
 # Your code goes below. Hints:
+address = 'the+corner+of+foster+and+sheridan'
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=false"
+raw_data = open(url_of_data_we_want).read
 
-# url_of_data_we_want = "???"
-# raw_data = ???
-# parsed_data = ???
+parsed_data = JSON.parse(raw_data)
+results = parsed_data["results"]
 
-# ...
-
+first = results[0]
+geometry = first["geometry"]
 # Let's store the latitude in a variable called 'the_latitude',
 #   and the longitude in a variable called 'the_longitude'.
 
-# the_latitude = ???
-# the_longitude = ???
+the_latitude = geometry["location"]["lat"]
+the_longitude = geometry["location"]["lng"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
+puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
